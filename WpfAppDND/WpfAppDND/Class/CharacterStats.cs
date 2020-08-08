60a58c112b9cdc4fded86b924765f562dcf4dd55
Liam-Saxon <60723434+Liam-Saxon@users.noreply.github.com>
2020-08-08 22:19:00 +0800

    Add files via upload
    Backup. No functionality as of yet.

---
+++ WpfAppDND/WpfAppDND/Class/CharacterStats.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppDND.Class
+{
+    class CharacterStats
+    {
+
+
+
+        //numbered stats from 10-whatever number they achieve
+        private readonly int strength;
+        private readonly int Dexterity;
+        private readonly int Constitution;
+        private readonly int Intelligence;
+        private readonly int Wisdom;
+        private readonly int Charisma;
+
+        public int StrGS { get; set; }
+
+        public int DexGS { get; set; }
+
+        public int ConGS { get; set; }
+
+        public int IntGS { get; set; }
+
+        public int WisGS { get; set; }
+
+        public int ChaGS { get; set; }
+    }
+}
